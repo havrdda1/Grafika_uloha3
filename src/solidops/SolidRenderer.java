e4b51a5e3cbf8de909a2da593280d367eafff640
havrdda1 <43078184+havrdda1@users.noreply.github.com>
2018-12-05 16:55:17 +0100

    Added interface SolidRenderer

---
+++ src/solidops/SolidRenderer.java
@@ -0,0 +1,39 @@
+package solidops;
+
+import imagedata.Image;
+import io.vavr.collection.IndexedSeq;
+import org.jetbrains.annotations.NotNull;
+import solidsdata.Topology;
+import solidsdata.Solid;
+import transforms.Mat4;
+
+public interface SolidRenderer<V, T, P> {
+    default @NotNull Image<P> render(
+            @NotNull Image<P> image,
+            @NotNull Solid<V, T> solid,
+            @NotNull Mat4 matTransform,
+            @NotNull P value
+    ) {
+        return solid.getParts()
+                    .foldLeft(image, (currentImage, part) -> render(currentImage,
+                                                                    solid.getVertices(),
+                                                                    solid.getIndices(),
+                                                                    part.getIndexStart(),
+                                                                    part.getPrimitivesCount(),
+                                                                    part.getTopology(),
+                                                                    matTransform,
+                                                                    value));
+    }
+
+    @NotNull
+    Image<P> render(
+            @NotNull Image<P> image,
+            @NotNull IndexedSeq<V> vertices,
+            @NotNull IndexedSeq<Integer> indices,
+            int indexStart,
+            int primitivesCount,
+            @NotNull T topology,
+            @NotNull Mat4 matTransforms,
+            @NotNull P value
+    );
+}
